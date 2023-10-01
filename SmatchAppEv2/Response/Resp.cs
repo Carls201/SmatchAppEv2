2fd940a456205511cbd926cf080a8545b46c15cd
Carlos Uribe <carlosuribe710@gmail.com>
2023-10-01 11:00:46 -0400

    Se agrego la carpeta respose con la clase resp

---
+++ SmatchAppEv2/Response/Resp.cs
@@ -0,0 +1,12 @@
+﻿namespace SmatchAppEv2.Response
+{
+    public class Resp
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+        public Resp() { 
+            Success = false; 
+        }
+    }
+}
